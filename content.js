3385190aa07c8f3d9fb568bf407773553ee3242f
İbrahim Erdoğan <kararlibiri@gmail.com>
2019-01-22 15:07:55 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,44 @@
+$ = this.$ = this.jQuery = jQuery.noConflict(true);
+$(document).ready(function() {
+	get_blogs();
+});
+function get_blogs() {
+	var blogs = new Array();
+	$('.tab_blogs .tab_blog:not(.tab_dashboard)').each(function() {
+		blogs.push($(this).attr('id').slice(9));
+	});
+	check_followings(blogs[0]);
+}
+function check_followings(me) {
+	$('div#following div.follower').each(function(index, element) {
+		var name = $('.info .name > a', this).html();
+		if (name == null || name == "") return;
+		$.ajax({
+			type: "GET",
+			url: "/svc/conversations/participant_info",
+			data: {
+				"participant": me + '.tumblr.com',
+				"q": name + '.tumblr.com'
+			},
+			contentType: 'application/x-www-form-urlencoded',
+			statusCode: {
+				200: function(data, textStatus, jqXHR) {
+					var div = $("<div />").addClass("tfc_description");
+					if (data.response.is_blog_following_you == true) {
+						div.html('Seni takip ediyor! <a title="Uygulamayı oluşturan: @duygusalyanim" href="http://duygusalyanim.tumblr.com">©</a>');
+						div.addClass("tfc_follower");
+					} else {
+						div.html('Seni takip etmiyor! <a title="Uygulamayı oluşturan: @duygusalyanim" href="http://duygusalyanim.tumblr.com">©</a>');
+						div.addClass("tfc_non_follower");
+					}
+					$('.info', element).append(div);
+				},
+				403: function(jqXHR, textStatus, errorThrown) {
+					data = $.parseJSON(jqXHR.responseText);
+					var div = $("<div />").addClass("tfc_not_sure").text("İkinci blogu seni takip etmiyor.");
+					$('.info', element).append(div);
+				}
+			}
+		});
+	});
+}
